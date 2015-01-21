a2bcd6b83aba43cf6dc22a63ad33d37aad63bc26
Jeff <jlee@fool.com>
2015-01-20 23:26:15 -0500

    use local cache on dev

--- project/settings/live.py
+++ project/settings/live.py
@@ -4,6 +4,16 @@ from project.settings.default import *
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
 # Make this unique, and don't share it with anybody.
 import os
 SECRET_KEY = os.environ['SECRET_KEY']
